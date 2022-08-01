910909a6584c72ea3f1a2b71410ab1820ea2f383
yinruiqing <yinruiqing110@gmail.com>
2022-08-01 22:58:13 +0800

    add: cfsmn and vfsmn

--- fsmn.py
+++ fsmn.py
@@ -17,7 +17,7 @@ class FSMN(nn.Module):
         nn.init.xavier_uniform_(self._W1)
         nn.init.xavier_uniform_(self._W2)
         nn.init.ones_(self._bias)
-        nn.init.ones_(self._memory_weights)
+        nn.init.xavier_uniform_(self._memory_weights)
 
     def get_memory_matrix(self, num_steps):
         memory_matrix = []
@@ -38,3 +38,56 @@ class FSMN(nn.Module):
         h += torch.matmul(h_hatt, self._W2) + self._bias
         return h
 
+
+class CSFSMN(FSMN):
+    def __init__(self, memory_size, input_size, output_size):
+        super().__init__(memory_size, input_size, output_size)
+        self._W1 = nn.Parameter(torch.Tensor(self.input_size, self.output_size))
+        self._W2 = nn.Parameter(torch.Tensor(self.input_size, self.output_size))
+        self._bias1 = nn.Parameter(torch.Tensor(self.project_size))
+        self._bias2 = nn.Parameter(torch.Tensor(self.output_size))
+        self._memory_weights = nn.Parameter(torch.Tensor(self.memory_size))
+
+        nn.init.xavier_uniform_(self._W1)
+        nn.init.xavier_uniform_(self._W2)
+        nn.init.ones_(self._bias1)
+        nn.init.ones_(self._bias2)
+        nn.init.xavier_uniform_(self._memory_weights)
+
+    def forward(self, input_data):
+        num_steps = input_data.size(1)
+        p = torch.matmul(input_data, self._W1) + self._bias1
+        memory_matrix = self.get_memory_matrix(num_steps)
+        p = torch.matmul(memory_matrix, p)
+        p = torch.matmul(p, self._W2) + self._bias2
+        return p
+
+
+class VFSMN(nn.Module):
+    def __init__(self, memory_size, input_size, output_size):
+        super().__init__()
+        self.memory_size = memory_size
+        self.output_size = output_size
+        self.input_size = input_size
+        self._W1 = nn.Parameter(torch.Tensor(self.input_size, self.output_size))
+        self._W2 = nn.Parameter(torch.Tensor(self.input_size, self.output_size))
+        self._bias = nn.Parameter(torch.Tensor(self.output_size))
+        self._memory_weights = nn.Parameter(torch.Tensor(self.memory_size + 1, self.input_size))
+
+        nn.init.xavier_uniform_(self._W1)
+        nn.init.xavier_uniform_(self._W2)
+        nn.init.ones_(self._bias)
+        nn.init.xavier_uniform_(self._memory_weights)
+        with torch.no_grad():
+            self._memory_weights[0] = 0
+
+    def forward(self, input_data):
+        num_steps = input_data.size(1)
+        memory_matrix = torch.ones((num_steps, num_steps), requires_grad=False).tril(-1).cumsum(0).triu(
+            - self._memory_size).long()
+        memory_matrix = memory_matrix.unsqueeze(0).expand(input_data.size(0), -1, -1)
+        memory = self._memory_weights[memory_matrix].to(self.device)
+        h_hatt = torch.einsum('bijd,bjd->bid', memory, input_data)  # 'bijd,bjd->bid'
+        h = torch.matmul(input_data, self._W1)
+        h += torch.matmul(h_hatt, self._W2) + self._bias
+        return h
